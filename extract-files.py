242ff06138fd58997ae8c4746a3a8e6a0158b4f0
LuK1337 <priv.luk@gmail.com>
2024-10-12 17:42:02 +0200

    sm6150-common: Shim WFD with libgui_shim
    Change-Id: I42bf06bc6b19942303031ef0147dafa6df127fe5
    Signed-off-by: Frost <frost.github@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -47,7 +47,8 @@ lib_fixups: lib_fixups_user_type = {
 
 blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
-        .add_needed('libaudiobase.so'),
+        .add_needed('libaudiobase.so')
+        .add_needed('libgui_shim.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libinput_shim.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
