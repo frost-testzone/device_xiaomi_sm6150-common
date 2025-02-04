13a2956718044927badf931f8c14a1b6fea0990c
basamaryan <basam.aryan@gmail.com>
2025-02-04 15:37:31 -0500

    sm6150-common: Remove /data/vendor/dolby on upgrade
    This is required after switching to the OnePlus Dolby Atmos stack
    
    Based on:
    https://github.com/adithya2306/android_device_xiaomi_lisa/commit/09e0c6ad23ec651b2281672463d360a9b2b87d7c
    https://github.com/adithya2306/android_device_xiaomi_K9-sepolicy/commit/346519f7f272e45dd2be08db51669ff3700b1f90
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I2ac8f83899a0866975aa6cbce26da9670b16843d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Frost <frost.github@proton.me>

--- sm6150.mk
+++ sm6150.mk
@@ -125,6 +125,10 @@ PRODUCT_COPY_FILES += \
 # Dolby
 $(call inherit-product, vendor/oneplus/dolby/dolby.mk)
 
+# Dolby Fix script
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/bin/init.dolby_fix.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.dolby_fix.sh
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
