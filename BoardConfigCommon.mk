fb1729e7d54c239a79652ffd669312a3403ae694
Frost <frost.github@proton.me>
2025-08-25 18:20:45 +0530

    sm6150-common: Include Dolby Board Config makefile
    Change-Id: I97cf8a00897e34e0044b962ba11a610e57b155ca
    Signed-off-by: Frost <frost.github@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,6 +44,9 @@ BOARD_SUPPORTS_SOUND_TRIGGER := true
 TARGET_BOOTLOADER_BOARD_NAME := sm6150
 TARGET_NO_BOOTLOADER := true
 
+# Dolby
+include vendor/oneplus/dolby/BoardConfigDolby.mk
+
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/configs/config.fs
 
