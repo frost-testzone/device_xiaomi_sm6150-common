6262fcd764f821d85d3b5947f85ee43408b63f0d
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-01 01:33:26 -0400

    sm6150-common: Use legacy libion implementation
    * At least one blob directly depends on this, with
      many other bits and pieces likely.
    
    Change-Id: Ic063618d6942a4f6e90dd7e21698e36d14113d06
    Signed-off-by: Frost <frost.github@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -135,6 +135,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
+include device/voltage/sepolicy/libion/sepolicy.mk
 include device/voltage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
